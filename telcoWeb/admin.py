bd798b90dd623c2574197a3fce04ddf2c1b550bf
catra <naufalcatra@gmail.com>
2021-06-17 20:37:59 +0700

    Modify database

--- telcoWeb/admin.py
+++ telcoWeb/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Informations
+from .models import Kategori,Materi,Informations
 
 # Register your models here.
+admin.site.register(Kategori)
+admin.site.register(Materi)
 admin.site.register(Informations)
